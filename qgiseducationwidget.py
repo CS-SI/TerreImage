4f325159c263d13dd2510578c067fbea7c6e878e
Alexia Mondot <alexia.mondot@c-s.fr>
2014-05-16 15:54:28 +0200

    gestion de langles spectral en commentaire

--- qgiseducationwidget.py
+++ qgiseducationwidget.py
@@ -55,13 +55,17 @@ class QGISEducationWidget(QtGui.QWidget, Ui_QGISEducation):
         self.working_directory, _ = terre_image_utils.fill_default_directory()
         self.value_tool = ValueWidget( self.iface, self )
         
-        self.tool = ProfiletoolMapTool(self.iface.mapCanvas())        #the mouselistener
-        print "self.tool", self.tool
-        self.pointstoDraw = None    #Polyline in mapcanvas CRS analysed
-        self.maptool = self.canvas.mapTool()
-        self.get_point_for_angles()
         self.layer = None
         self.memorylayer = None
+        
+        
+        self.angle_tool = SpectralAngle(self.iface, self.working_directory, self.layer)
+#         self.tool = ProfiletoolMapTool(self.iface.mapCanvas())        #the mouselistener
+#         print "self.tool", self.tool
+#         self.pointstoDraw = None    #Polyline in mapcanvas CRS analysed
+#         self.maptool = self.canvas.mapTool()
+#         #self.get_point_for_angles()
+
      
     def setupUi_extra(self):
         """
@@ -90,7 +94,8 @@ class QGISEducationWidget(QtGui.QWidget, Ui_QGISEducation):
 #         self.pushButton_ndvi.clicked.connect(self.ndvi)
 #         self.pushButton_working_layer.clicked.connect(self.working_layer)
 #         self.pushButton_angle.clicked.connect(self.spectral_angles)
-        
+
+        self.pushButton_kmeans.clicked.connect(self.kmeans)
         
         
     def do_manage_processing(self, text_changed):
@@ -112,11 +117,16 @@ class QGISEducationWidget(QtGui.QWidget, Ui_QGISEducation):
         
             self.comboBox_sprectral_band_display.clear()
             self.comboBox_sprectral_band_display.insertItem( 0, "" )
+            
+            if bands['red'] != 0 and bands['green'] != 0 and bands['blue'] != 0:
+                print "couleurs naturelles"
+                self.comboBox_sprectral_band_display.insertItem( 1, "Afficher en couleurs naturelles" )
+            
             for i in range(self.layer.get_band_number()):
                 y=[x for x in bands if bands[x]==i+1]
                 if y :
                     text = corres[y[0]]
-                    self.comboBox_sprectral_band_display.insertItem( i+1, text )
+                    self.comboBox_sprectral_band_display.insertItem( i+2, text )
             self.comboBox_sprectral_band_display.currentIndexChanged[str].connect(self.do_manage_sprectral_band_display)
             
     def do_manage_sprectral_band_display(self, text_changed):
@@ -126,8 +136,8 @@ class QGISEducationWidget(QtGui.QWidget, Ui_QGISEducation):
             if corres[key] == text_changed :
                 who = key
                 band_to_display = self.layer.bands[key]
+                manage_QGIS.display_one_band(self.layer, who, self.iface)
                 break
-        manage_QGIS.display_one_band(self.layer, who, self.iface)
         
         
         
@@ -150,7 +160,15 @@ class QGISEducationWidget(QtGui.QWidget, Ui_QGISEducation):
             print "Aucune layer selectionnée"
         else :
             terre_image_processing.ndti(self.layer, self.working_directory, self.iface)
-
+                
+                 
+    def kmeans(self):
+        if self.layer == None :
+            print "Aucune layer selectionnée"
+        else :
+            nb_class = self.spinBox_kmeans.value()
+            print "nb_colass from spinbox", nb_class
+            terre_image_processing.kmeans(self.layer, self.working_directory, self.iface, nb_class)
         
         
     def working_layer(self):
@@ -161,55 +179,54 @@ class QGISEducationWidget(QtGui.QWidget, Ui_QGISEducation):
         
     
     def spectral_angles( self ):
-        self.get_point_for_angles()
+        self.angle_tool.get_point_for_angles(self.layer)
+#         self.get_point_for_angles()
 #         if self.layer :
 #             angle_tool = SpectralAngle(self.iface, self.layer)
         
    
-    def angles(self, x, y):
-        if self.layer :
-            terre_image_processing.angles(self.layer, self.working_directory, self.iface, x, y)
-        self.tool.deactivate()
-        self.deactivate()
-        self.canvas.setMapTool(self.maptool) 
-                    
-                    
-                    
-                    
-    
-    def get_point_for_angles(self):
-        print "get point for angles"
-        QtCore.QObject.connect(self.tool, QtCore.SIGNAL("canvas_clicked"), self.rightClicked)
-        
-        #init the mouse listener comportement and save the classic to restore it on quit
-        self.canvas.setMapTool(self.tool)
-        
-        print "listener set"
-        
-        #init the temp layer where the polyline is draw
-        self.polygon = False
-        self.rubberband = QgsRubberBand(self.canvas, self.polygon)
-        self.rubberband.setWidth(10)
-        self.rubberband.setColor(QtGui.QColor(QtCore.Qt.red))
-        #init the table where is saved the poyline
-        self.pointstoDraw = []
-        self.pointstoCal = []
-        
-    def deactivate(self):        #enable clean exit of the plugin
-        QtCore.QObject.disconnect(self.tool, QtCore.SIGNAL("canvas_clicked"), self.rightClicked)
-        #self.rubberband.reset(self.polygon)
-        
-        
-    def rightClicked(self,position):    #used to quit the current action
-        mapPos = self.canvas.getCoordinateTransform().toMapCoordinates(position["x"],position["y"])
-        newPoints = [[mapPos.x(), mapPos.y()]]
-        #if newPoints == self.lastClicked: return # sometimes a strange "double click" is given
-        self.memorylayer = manage_QGIS.show_clicked_point( QgsPoint(mapPos.x(),mapPos.y()), "spectral_angles_points", self.iface, self.memorylayer )
-        #self.rubberband.reset(self.polygon)
-        #self.rubberband.reset(QGis.Point)
-        self.rubberband.addPoint(QgsPoint(mapPos.x(),mapPos.y()))
-        #create new vlayer ???
-        self.angles(mapPos.x(),mapPos.y())
+#     def angles(self, x, y):
+#         if self.layer :
+#             terre_image_processing.angles(self.layer, self.working_directory, self.iface, x, y)
+#         self.tool.deactivate()
+#         self.deactivate()
+#         self.canvas.setMapTool(self.maptool) 
+#                     
+#                     
+#     
+#     def get_point_for_angles(self):
+#         print "get point for angles"
+#         QtCore.QObject.connect(self.tool, QtCore.SIGNAL("canvas_clicked"), self.rightClicked)
+#         
+#         #init the mouse listener comportement and save the classic to restore it on quit
+#         self.canvas.setMapTool(self.tool)
+#         
+#         print "listener set"
+#         
+#         #init the temp layer where the polyline is draw
+#         self.polygon = False
+#         self.rubberband = QgsRubberBand(self.canvas, self.polygon)
+#         self.rubberband.setWidth(10)
+#         self.rubberband.setColor(QtGui.QColor(QtCore.Qt.yellow))
+#         #init the table where is saved the poyline
+#         self.pointstoDraw = []
+#         self.pointstoCal = []
+#         
+#     def deactivate(self):        #enable clean exit of the plugin
+#         QtCore.QObject.disconnect(self.tool, QtCore.SIGNAL("canvas_clicked"), self.rightClicked)
+#         #self.rubberband.reset(self.polygon)
+#         
+#         
+#     def rightClicked(self,position):    #used to quit the current action
+#         mapPos = self.canvas.getCoordinateTransform().toMapCoordinates(position["x"],position["y"])
+#         newPoints = [[mapPos.x(), mapPos.y()]]
+#         #if newPoints == self.lastClicked: return # sometimes a strange "double click" is given
+#         self.memorylayer = manage_QGIS.show_clicked_point( QgsPoint(mapPos.x(),mapPos.y()), "spectral_angles_points", self.iface, self.memorylayer )
+#         #self.rubberband.reset(self.polygon)
+#         #self.rubberband.reset(QGis.Point)
+#         self.rubberband.addPoint(QgsPoint(mapPos.x(),mapPos.y()))
+#         #create new vlayer ???
+#         self.angles(mapPos.x(),mapPos.y())
    
         
         
