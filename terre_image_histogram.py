9cf7cbc316178cc964d3d21c221ffe64da4a0f4f
Alexia Mondot <alexia.mondot@c-s.fr>
2014-06-02 20:38:15 +0200

    generation dhistogrammes avec les droites min max interactives

--- terre_image_histogram.py
+++ terre_image_histogram.py
@@ -54,17 +54,17 @@ import random
 
 
 class MyMplCanvas(FigureCanvas):
+    __pyqtSignals__ = ("valueChanged()")
+    
     """Ultimately, this is a QWidget (as well as a FigureCanvasAgg, etc.)."""
     def __init__(self, parent=None, width=5, height=4, dpi=100):
-        
+       
         fig = Figure(figsize=(width, height), dpi=dpi)
         self.axes = fig.add_subplot(111)
         # We want the axes cleared every time plot() is called
+
         self.axes.hold(False)
 
-        #self.compute_initial_figure()
-
-        #
         FigureCanvas.__init__(self, fig)
         self.setParent(parent)
 
@@ -72,15 +72,12 @@ class MyMplCanvas(FigureCanvas):
                                    QtGui.QSizePolicy.Expanding,
                                    QtGui.QSizePolicy.Expanding)
         FigureCanvas.updateGeometry(self)
-        
-        self.rect = Rectangle((0,0), 1, 1)
-        self.x0 = None
-        self.y0 = None
-        self.x1 = None
-        self.y1 = None
 
-    def compute_initial_figure(self):
-        pass
+        self.two_min = 0
+        self.ninety_eight_max = 0
+        self.x_min = 0
+        self.x_max = 0
+        self.change_min = True
 
 
     def get_GDAL_histogram( self, image, band ):
@@ -88,177 +85,200 @@ class MyMplCanvas(FigureCanvas):
         From the given binary image, compute histogram and return the number of 1
         """
         histogram = []
+        print "image", image
         dataset = gdal.Open(str(image), gdal.GA_ReadOnly)
         if dataset is None:
             print "Error : Opening file ", image
         else :
             band = dataset.GetRasterBand(band)
             rasterMin, rasterMax = band.ComputeRasterMinMax()
-            nbVal = rasterMax - rasterMin
+            nbVal = rasterMax# - rasterMin
             if nbVal < 1 :
                 return 0
             #get the histogram of the given raster
-            histogram = band.GetHistogram(rasterMin, rasterMax+1, int(nbVal+1), approx_ok = 0)
+            #histogram = band.GetHistogram(rasterMin, rasterMax+1, int(nbVal+1), approx_ok = 0)
+            histogram = band.GetHistogram(0, rasterMax+1, int(nbVal+1), approx_ok = 0)
+            #print "histogram", histogram
+            
+            # get 2 - 98 %
+            #taking the size of the raster
+            sizeX = dataset.RasterXSize
+            sizeY = dataset.RasterYSize
+            print "sizeX, sizeY", sizeX, sizeY
+            nb_pixels = sizeX * sizeY
+            print nb_pixels
+            
+            nb_pixels_2 = int(float(nb_pixels * 0.02))
+            nb_pixels_98 = int(float(nb_pixels * 0.98))
+            
+            print "nb_pixels_2, nb_pixels_98", nb_pixels_2, nb_pixels_98
+            
+            # catch the i where cum_hist(i) > nb_pixels_2 and nb_pixels_98
+            hist_cum = 0
+            for i in range(len(histogram)):
+                #print "i", i
+                if hist_cum > nb_pixels_2 and self.x_min == 0 :
+                    self.x_min = i #+ rasterMin
+                if hist_cum > nb_pixels_98 :
+                    self.x_max = i #+ rasterMin
+                    break;
+                hist_cum += histogram[i]
+            print "self.x_min, self.x_max", self.x_min, self.x_max
+            self.two_min = self.x_min
+            self.ninety_eight_max = self.x_max
+            print "self.two_min, self.ninety_eight_max", self.two_min, self.ninety_eight_max
+
             return histogram
         
-
-class MyStaticMplCanvas(MyMplCanvas):
-    """Simple canvas with a sine plot."""
-    def compute_initial_figure(self, raster_matrix, color_c='k'):
-        flat_raster = ma.compressed(raster_matrix)
-        #ax.hist(self, x, bins=10, range=None, normed=False, weights=None, cumulative=False, bottom=None, histtype='bar', align='mid', orientation='vertical', rwidth=None, log=False, color=None, label=None, **kwargs)
-        #self.axes.hist(flat_raster, 500, normed=0, histtype='bar', align='mid', color=color_c)
-        self.axes.hist(flat_raster, 500, normed=0, histtype='stepfilled', align='mid', color=color_c)
-        #t = arange(0.0, 3.0, 0.01)
-        #s = sin(2*pi*t)
-        #self.axes.plot(t, s)
-        
     def display_histogram(self, filename, band, color, name):
+        self.color = color
+        self.name = name
         histogram = self.get_GDAL_histogram(filename, band)
         #print histogram
-        t = range(0, len(histogram))
-        s = histogram
-        self.axes.plot(t, s, color)
+        self.t = range(0, len(histogram))
+        self.s = histogram
+        self.draw_histogram()
         self.axes.figure.canvas.mpl_connect('button_press_event', self.on_press)
         self.axes.figure.canvas.mpl_connect('button_release_event', self.on_release)
-        xtext = self.axes.set_xlabel('Valeur') # returns a Text instance
-        ytext = self.axes.set_ylabel('Nombre')
-        self.axes.set_title(name)
+        self.draw_min_max_percent()
+        
+        
+    def draw_histogram(self):
+        if self.t and self.s and self.color and self.name:
+            self.axes.plot(self.t, self.s, self.color)
+            xtext = self.axes.set_xlabel('Valeur') # returns a Text instance
+            ytext = self.axes.set_ylabel('Nombre')
+            self.axes.set_title(self.name)
+            
+    def draw_reset_percent(self):
+        print "draw reset"
+        self.axes.clear()
+        self.draw_histogram()
+        if self.two_min and self.ninety_eight_max:
+             print "self.two_min, self.ninety_eight_max", self.two_min, self.ninety_eight_max
+             self.axes.axvline(x=self.two_min,c="red",linewidth=2,zorder=0, clip_on=False)
+             self.axes.axvline(x=self.ninety_eight_max,c="red",linewidth=2,zorder=0, clip_on=False)
+        self.axes.figure.canvas.draw()
+        self.x_min = self.two_min
+        self.x_max = self.ninety_eight_max 
+        self.emit( QtCore.SIGNAL("valueChanged()") )
+
+
+    def draw_min_max_percent(self):
+        if self.x_min and self.x_max:
+            print "self.x_min, self.x_max", self.x_min, self.x_max
+            self.axes.axvline(x=self.x_min,c="red",linewidth=2,zorder=0, clip_on=False)
+            self.axes.axvline(x=self.x_max,c="red",linewidth=2,zorder=0, clip_on=False)
+            
         
     def on_press(self, event):
+        """
+        Check if the clicked point is nearer from x_min than from x_max
+        """
         print 'press'
-        self.x0 = event.xdata
-        self.y0 = event.ydata
+        x = event.xdata
+        # take x_min
+        if abs(x-self.x_min) < abs(x-self.x_max):
+            self.change_min = True
+        else:
+            self.change_min = False
+        #self.valueChanged.emit()
+        
+        
+        #self.x_min = 
 
     def on_release(self, event):
+        """
+        Check
+        """
         print 'release'
-        self.x1 = event.xdata
-        self.y1 = event.ydata
-        self.rect.set_width(self.x1 - self.x0)
-        self.rect.set_height(self.y1 - self.y0)
-        self.rect.set_xy((self.x0, self.y0))
+        if self.change_min :
+            self.x_min = event.xdata
+        else:
+            self.x_max = event.xdata
+        
+        self.axes.clear()
+        self.draw_histogram()
+        #self.axes.plot(self.t, self.s, self.color)
+        #self.axes.plot(self.x1, self.x1, 'g^')
+        self.draw_min_max_percent()
+        #self.axes.vlines(self.x1, [0], self.x1)
+#         self.axes.set_xlabel('time (s)')
+#         self.axes.set_title('Vertical lines demo')
+        
         self.axes.figure.canvas.draw()
-        print self.x0, self.x1, self.y0, self.y1
-
-
-class MyDynamicMplCanvas(MyMplCanvas):
-    """A canvas that updates itself every second with a new plot."""
-    def __init__(self, *args, **kwargs):
-        MyMplCanvas.__init__(self, *args, **kwargs)
-        timer = QtCore.QTimer(self)
-        QtCore.QObject.connect(timer, QtCore.SIGNAL("timeout()"), self.update_figure)
-        timer.start(1000)
-
-    def compute_initial_figure(self):
-         self.axes.plot([0, 1, 2, 3], [1, 2, 0, 4], 'r')
-
-    def update_figure(self):
-        # Build a list of 4 random integers between 0 and 10 (both inclusive)
-        l = [ random.randint(0, 10) for i in range(4) ]
-
-        self.axes.plot([0, 1, 2, 3], l, 'r')
-        self.draw()
+        self.emit( QtCore.SIGNAL("valueChanged()") )
+        print self.x_min, self.x_max
 
 
 
-class TerreImageHistogram(QtGui.QWidget) :#, Ui_Form):
+class TerreImageHistogram(QtGui.QWidget, QtCore.QObject) :#, Ui_Form):
     
-    __pyqtSignals__ = ("curveTitleChanged(str)", "hideCurve(int)", "colorChanged(QtGui.QColor)", "deleteCurve()")
+    #__pyqtSignals__ = ("curveTitleChanged(str)", "hideCurve(int)", "colorChanged(QtGui.QColor)", "deleteCurve()")
+    __pyqtSignals__ = ("valueChanged(PyQt_PyObject)", "threshold(PyQt_PyObject)")
     
     
     def __init__(self, layer):
-        QWidget.__init__(self)
+        QtGui.QWidget.__init__(self)
+        QtCore.QObject.__init__(self)
         #self.setupUi(self)
         
         self.layer = layer
         
         
         l = QVBoxLayout(self)
-#         sc = MyStaticMplCanvas(self, width=5, height=4, dpi=100)
-#         l.addWidget(sc)
+        self.sc_1 = MyMplCanvas(self, width=5, height=4, dpi=100)
+        #self.sc_1.valueChanged.connect( self.valueChanged )
+        QtCore.QObject.connect( self.sc_1, QtCore.SIGNAL( "valueChanged()" ), self.valueChanged )
+        self.sc_2 = MyMplCanvas(self, width=5, height=4, dpi=100)
+        QtCore.QObject.connect( self.sc_2, QtCore.SIGNAL( "valueChanged()" ), self.valueChanged )
+        self.sc_3 = MyMplCanvas(self, width=5, height=4, dpi=100)
+        QtCore.QObject.connect( self.sc_3, QtCore.SIGNAL( "valueChanged()" ), self.valueChanged )
+        l.addWidget(self.sc_1)
+        l.addWidget(self.sc_2)
+        l.addWidget(self.sc_3)
+        b = QtGui.QPushButton("Remise à zéro")
+        b.clicked.connect(self.reset)
+        l.addWidget(b)
+        seuil = QtGui.QPushButton("Seuillage")
+        seuil.clicked.connect(self.seuillage)
+        l.addWidget(seuil)
         
         
-        sc_1 = MyStaticMplCanvas(self, width=5, height=4, dpi=100)
-        sc_2 = MyStaticMplCanvas(self, width=5, height=4, dpi=100)
-        sc_3 = MyStaticMplCanvas(self, width=5, height=4, dpi=100)
-        #dc = MyDynamicMplCanvas(self, width=5, height=4, dpi=100)
-        l.addWidget(sc_1)
-        l.addWidget(sc_2)
-        l.addWidget(sc_3)
-        #l.addWidget(dc)
 
         #file_pointer = rasterIO.opengdalraster(layer.get_source())
         # pir, red, green
         print "layer pir", layer.pir
         #band = rasterIO.readrasterband(file_pointer, 1)
         #sc_1.compute_initial_figure(band, 'r')
-        sc_1.display_histogram(layer.get_source(), layer.pir, 'r', "Plan R : BS PIR")
+        self.sc_1.display_histogram(layer.get_source(), layer.pir, 'r', "Plan R : BS PIR")
         #band = rasterIO.readrasterband(file_pointer, 2)
         #sc_2.compute_initial_figure(band, 'g')
-        sc_2.display_histogram(layer.get_source(), layer.red, 'g', "Plan V : BS R")
+        self.sc_2.display_histogram(layer.get_source(), layer.red, 'g', "Plan V : BS R")
         #band = rasterIO.readrasterband(file_pointer, 3)
         #sc_3.compute_initial_figure(band, 'b')
-        sc_3.display_histogram(layer.get_source(), layer.green, 'b', "Plan B : BS V")
+        self.sc_3.display_histogram(layer.get_source(), layer.green, 'b', "Plan B : BS V")
         #dc.compute_initial_figure()
         
+    def reset(self):
+        print "resset"
+        self.sc_1.draw_reset_percent()
+        self.sc_2.draw_reset_percent()
+        self.sc_3.draw_reset_percent()
+        
+    def seuillage(self):
+        forms = []
+        forms.append( "if(((im1b1>" + str(self.sc_1.x_min) + ") and (im1b1<" + str(self.sc_1.x_max) + ")), im1b1, 0)" )
+        forms.append( "if(((im1b2>" + str(self.sc_2.x_min) + ") and (im1b2<" + str(self.sc_2.x_max) + ")), im1b2, 0)" )
+        forms.append( "if(((im1b3>" + str(self.sc_3.x_min) + ") and (im1b3<" + str(self.sc_3.x_max) + ")), im1b3, 0)" )
+        print forms
+        #emit signal
+        self.emit( QtCore.SIGNAL("threshold(PyQt_PyObject)"), forms )
+        
+    def valueChanged(self):
+        print "value changed"
+        values = [ (self.sc_1.x_min, self.sc_1.x_max ), (self.sc_2.x_min, self.sc_2.x_max ), (self.sc_3.x_min, self.sc_3.x_max )]
+        print "values", values
+        self.emit( QtCore.SIGNAL("valueChanged(PyQt_PyObject)"), values )
         
         
-        
-#         # matplotlib context:
-#         self.mplFig = plt.figure(figsize=(8,11)) #facecolor='w', edgecolor='w', 
-#          
-#         # qt stuff
-#         self.pltCanvas = FigureCanvasQTAgg(self.mplFig)
-#         self.pltCanvas.setParent(self)
-#         self.pltCanvas.setAutoFillBackground(False)
-#         self.pltCanvas.setObjectName("mplPlot")
-#         self.mplPlot = self.pltCanvas
-#         self.mplPlot.setVisible(False)
-#         self.addWidget(self.mplPlot)
-#         xmin, xmax = xlim()
-#         print "xmin, xman", xmin, xmax
-#          
-#         # Open a raster using rasterIO
-#         # Open a gdal pointer to a file on disk
-#         file_pointer = rasterIO.opengdalraster('/home/amondot/OTB-Data/Examples/QB_1_ortho.tif')
-#         band = rasterIO.readrasterband(file_pointer, 1)
-#         # Pass matrix representation of raster to function
-#         self.rasterHistogram(band)
-#         # Shows histogram on screen
-        
- 
-    # Create a histogram function
-    def rasterHistogram(self, raster_matrix):
-        '''Accepts matrix and generates histogram'''
-         
-        # Line above is function docstring
-        # Flatten 2d-matrix
-        flat_raster = ma.compressed(raster_matrix)
-         
-        # Setup the plot (see matplotlib.sourceforge.net)
-        ax = self.mplFig.add_subplot(1,1,1)
-         
-        # Plot histogram
-        #ax.hist(self, x, bins=10, range=None, normed=False, weights=None, cumulative=False, bottom=None, histtype='bar', align='mid', orientation='vertical', rwidth=None, log=False, color=None, label=None, **kwargs)
-        ax.hist(flat_raster, 100, normed=0, histtype='bar', align='mid')
-        # Show the plot on screen
-        plt.show()
-     
-
-        
-        
-        
-#         # should make figure light gray
-#         self.mplBackground = None #http://www.scipy.org/Cookbook/Matplotlib/Animations
-#         self.mplFig = plt.Figure(facecolor='w', edgecolor='w')
-#         self.mplFig.subplots_adjust(left=0.1, right=0.975, bottom=0.13, top=0.95)
-#         self.mplPlt = self.mplFig.add_subplot(111)   
-#         self.mplPlt.tick_params(axis='both', which='major', labelsize=12)
-#         self.mplPlt.tick_params(axis='both', which='minor', labelsize=10)                           
-#         # qt stuff
-#         self.pltCanvas = FigureCanvasQTAgg(self.mplFig)
-#         self.pltCanvas.setParent(self.stackedWidget)
-#         self.pltCanvas.setAutoFillBackground(False)
-#         self.pltCanvas.setObjectName("mplPlot")
-#         self.mplPlot = self.pltCanvas
-#         self.mplPlot.setVisible(False)
