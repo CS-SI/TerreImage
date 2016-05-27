b5292359636211de63927dc23b1569a8284158f4
Alexia Mondot <alexia.mondot@c-s.fr>
2016-05-27 11:28:07 +0200

    TEST complete tests on working_layer

--- tests/src/test_terre_image_working_layer.py
+++ tests/src/test_terre_image_working_layer.py
@@ -33,10 +33,29 @@ class TestWorkingLayer(TerreImageTestCase):
         self.image_test = os.path.join(self.data_dir_input, "taredji_extract.TIF")
 
     def testWorkingLayerWithoutQGISLayer_ok(self):
-        self.terre_image_layer = WorkingLayer(self.image_test, None, {'red':1, 'green':2, 'blue':3, 'pir':4, 'mir':None})
+        """
+        Test on Terre Image working layer creation
+        Returns:
+
+        """
+        self.terre_image_layer = WorkingLayer(self.image_test, self.raster_layer, {'red':1, 'green':2, 'blue':3, 'pir':4, 'mir':None})
+        self.assertIsNot( self.terre_image_layer.get_qgis_layer(), None)
+        self.assertIs(self.terre_image_layer.get_source(), self.image_test)
+        # while the layer is not loaded in qgis, this information is not available
+        self.assertEqual(self.terre_image_layer.get_band_number(),0)
+        self.assertEqual(self.terre_image_layer.has_natural_colors(), True)
+        # can not test the whole string because of the dictionary
+        print_message_start = "{} taredji_extract".format(self.image_test)
+        generated_printed_message = str(self.terre_image_layer)
+        self.assertTrue(generated_printed_message.startswith(print_message_start))
 
 
     def testWorkingLayerWithoutQGISLayer_colorMissingdef(self):
+        """
+        Def test on bad structure in Terre Image working layer creation
+        Returns:
+
+        """
         with self.assertRaises(KeyError):
           self.terre_image_layer = WorkingLayer(self.image_test, None, {'red':1, 'green':2, 'blue':3, 'pir':4})
 
