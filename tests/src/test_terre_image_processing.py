139f7c10fab6e01479487cc1ca6971926e803137
Alexia Mondot <alexia.mondot@c-s.fr>
2016-05-27 10:21:22 +0200

    ENH add tests

--- tests/src/test_terre_image_processing.py
+++ tests/src/test_terre_image_processing.py
@@ -31,26 +31,98 @@ from terre_image_test_case import TerreImageTestCase
 class TestTerreImageProcessingMethods(TerreImageTestCase):
 
     def setUp(self):
+        """
+        Defines the test image and create a Terre Image working layer
+        Returns:
+
+        """
         self.image_test = os.path.join(self.data_dir_input, "taredji_extract.TIF")
         self.terre_image_layer = WorkingLayer(self.image_test, None, {'red':1, 'green':2, 'blue':3, 'pir':4, 'mir':None})
 
     def test_ndvi(self):
+        """
+        Test the ndvi function
+        Returns:
+
+        """
         generated_image = terre_image_processing.ndvi(self.terre_image_layer, self.working_dir)
         baseline = os.path.join(self.data_dir_baseline, "taredji_extract_ndvi.tif")
         self.assertTrue(self.checkResult(generated_image, baseline))
 
     def test_ndti(self):
+        """
+        Test the ndti function
+        Returns:
+
+        """
         generated_image = terre_image_processing.ndti(self.terre_image_layer, self.working_dir)
         baseline = os.path.join(self.data_dir_baseline, "taredji_extract_ndti.tif")
         self.assertTrue(self.checkResult(generated_image, baseline))
 
     def test_brightness(self):
+        """
+        Test the brightness function
+        Returns:
+
+        """
         generated_image = terre_image_processing.brightness(self.terre_image_layer, self.working_dir)
         baseline = os.path.join(self.data_dir_baseline, "taredji_extract_brightness.tif")
         self.assertTrue(self.checkResult(generated_image, baseline))
 
+    def test_threshold(self):
+        """
+        Test the threshold function
+        Returns:
 
+        """
+        generated_image = terre_image_processing.brightness(self.terre_image_layer, self.working_dir)
+        baseline = os.path.join(self.data_dir_baseline, "taredji_extract_brightness.tif")
+        self.assertTrue(self.checkResult(generated_image, baseline))
 
+    def test_angles(self):
+        """
+        Test the spectral angle function
+        Returns:
+
+        """
+        generated_image = terre_image_processing.brightness(self.terre_image_layer, self.working_dir)
+        baseline = os.path.join(self.data_dir_baseline, "taredji_extract_brightness.tif")
+        self.assertTrue(self.checkResult(generated_image, baseline))
+
+    def test_kmeans(self):
+        """
+        Test the kmeans function
+        Returns:
+
+        """
+        generated_image = terre_image_processing.kmeans(self.terre_image_layer, self.working_dir, 5)
+        baseline = os.path.join(self.data_dir_baseline, "taredji_extract_5classes_colored.tif")
+        self.assertTrue(self.checkResult(generated_image, baseline))
+        self.assertTrue(self.checkResult(generated_image, baseline), 2)
+        self.assertTrue(self.checkResult(generated_image, baseline), 3)
+
+    def test_get_sensor_id(self):
+        """
+        Test the function to get a sensor
+        Returns:
+
+        """
+        generated_sensor = terre_image_processing.get_sensor_id(self.terre_image_layer.source_file)
+        self.assertEqual(generated_sensor, None)
+        qb_1_extract = os.path.join(self.data_dir_input, "qb_1_ortho_extract.tif")
+        generated_sensor = terre_image_processing.get_sensor_id(qb_1_extract)
+        self.assertEqual(generated_sensor, "QB02")
+
+    def test_gdal_translate_get_one_band(self):
+        """
+        Test the gdal function to get a specific band
+        Returns:
+
+        """
+        generated_image = terre_image_processing.gdal_translate_get_one_band(self.terre_image_layer.source_file,
+                                                                             1, self.working_dir)
+        baseline = os.path.join(self.data_dir_baseline, "taredji_extract_b1.tif")
+        self.assertTrue(self.checkResult(generated_image, baseline))
 
 
 if __name__ == '__main__':
