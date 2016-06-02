7540122445aeaa354790644c5b65d0d1853a9133
Mickaël Savinaud <mickael.savinaud@c-s.fr>
2016-06-02 15:02:24 +0200

    ENH: fix the compilation to the remote module

--- OTB_TerreImage/otb-module.cmake
+++ OTB_TerreImage/otb-module.cmake
@@ -1,1 +1,1 @@
-otb_module(TerreImage DEPENDS OTBImageIO OTBStatistics DESCRIPTION "Additional app for TerreImage QGIS plugin")
+otb_module(TerreImage DEPENDS OTBImageIO OTBStatistics OTBApplicationEngine DESCRIPTION "Additional app for TerreImage QGIS plugin")
